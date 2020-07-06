cbf63ccf7c2879246404f9fc69f2a343c2613008
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-06 18:39:51 -0500

    adding create and show actions

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -4,4 +4,21 @@ class Api::ContactsController < ApplicationController
     @contacts = Contact.all
     render "index.json.jb"
   end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render 'show.json.jb'
+  end 
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render 'show.json.jb'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
  
   namespace :api do
     get "/contacts" => "contacts#index"
+    get "/contacts/:id" => "contacts#show"
+    post "/contacts" => "contacts#create"
   end
 end
